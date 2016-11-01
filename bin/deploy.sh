4caad168ff9925a3e6db80b5f5f67be25ec139c4
benniekrijger <benniekrijger@hotmail.com>
2016-11-01 18:47:18 +0100

    feat(ansible): [TC-2822] Improved playbooks

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,4 +6,4 @@ VERSION=`perl -n -e '/\s*version\s*:=\s*"(.*)"/ && print "$1\n"' < $PROJECT_DIR/
 
 echo "Deploying version ${VERSION}"
 
-ansible-playbook $PROJECT_DIR/deployment/deploy.yml -i $PROJECT_DIR/deployment/hosts -v --tags deploy --extra-vars "version=${VERSION}" -k -u vagrant
+ansible-playbook $PROJECT_DIR/deployment/deploy.yml -i $PROJECT_DIR/deployment/hosts -v --extra-vars "version=${VERSION}" -k -u vagrant
